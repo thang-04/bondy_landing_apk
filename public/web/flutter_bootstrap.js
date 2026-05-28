0450073f5cbabc8bdb58eedc508861452d980b32
Thangnd04 <thangnd04@gmail.com>
2026-05-28 22:34:46 +0700

    feat: compile Flutter web with base-href /web/ and update web assets

--- public/web/flutter_bootstrap.js
+++ public/web/flutter_bootstrap.js
@@ -34,5 +34,4 @@ if (!window._flutter) {
   window._flutter = {};
 }
 _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48ab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
-
 _flutter.loader.load();
