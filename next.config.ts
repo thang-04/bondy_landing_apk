ad611aa2d53ad466b82ee41b1ccd46740e2e907d
Thangnd04 <thangnd04@gmail.com>
2026-05-28 22:58:05 +0700

    feat: configure Vercel API proxy and update web assets with dynamic web base URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: "http://103.149.86.25:3000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
