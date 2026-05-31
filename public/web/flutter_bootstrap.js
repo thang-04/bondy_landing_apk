fd461e07b874d7b9a475a0f264827381e2dac77b
Thangnd04 <thangnd04@gmail.com>
2026-05-31 21:38:01 +0700

    build(web): recompile and update Flutter Web build with latest UI modifications (remove loading gradient, center survey intro graphic)

--- public/web/flutter_bootstrap.js
+++ public/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4158396598" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1734136531" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
