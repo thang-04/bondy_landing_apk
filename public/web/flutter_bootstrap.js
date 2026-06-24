bdcc06007ab50840a6f4f8be2b9e10d392a123b3
Thangnd04 <thangnd04@gmail.com>
2026-06-24 12:02:43 +0700

    build: rebuild web and APK with latest app updates

--- public/web/flutter_bootstrap.js
+++ public/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1729459922" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2013983841" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
